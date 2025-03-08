8104f5a03bb1324af58b8d5f1625d12213e2d898
Jose Leon <leonj1@gmail.com>
2025-03-08 16:33:28 +0000

    FIX define methods that are undefined

--- js/modules/Person.js
+++ js/modules/Person.js
@@ -1,6 +1,6 @@
-import { OBJECT_POOL } from '../game.js';
-import { generateRandomName } from './utils.js';
-import { THOUGHTS } from './translations.js';
+import { OBJECT_POOL, currentGenerationNumber } from '../game.js';
+import { generateRandomName, randomInt, sample } from './utils.js';
+import { THOUGHTS, getThought } from './translations.js';
 
 export const TRAITS = {
     FERTILE: 'Fertile',
@@ -9,6 +9,17 @@ export const TRAITS = {
     AUTISTIC: 'Autistic',
     PTSD: 'PTSD'
 };
+
+const OCCUPATIONS = [
+    'Farmer',
+    'Merchant',
+    'Teacher',
+    'Doctor',
+    'Builder',
+    'Guard',
+    'Priest',
+    'Artist'
+];
 
 export class Person {
     constructor(x, y, gender) {
@@ -61,6 +72,87 @@ export class Person {
         this.currentThought = this.generateThought();
         this.thoughtUpdateTimer = Math.random() * 5000 + 5000;
         this.traits = this.generateTraits();
+    }
+
+    assignOccupation() {
+        // Weight occupations based on town needs and current distribution
+        if (this.town) {
+            const occupationCounts = new Map();
+            this.town.people.forEach(person => {
+                if (person.occupation !== 'Child') {
+                    occupationCounts.set(person.occupation, (occupationCounts.get(person.occupation) || 0) + 1);
+                }
+            });
+
+            // Prioritize underrepresented occupations
+            const minCount = Math.min(...Array.from(occupationCounts.values(), count => count || 0));
+            const neededOccupations = OCCUPATIONS.filter(occ => !occupationCounts.has(occ) || occupationCounts.get(occ) === minCount);
+            
+            if (neededOccupations.length > 0) {
+                return sample(neededOccupations);
+            }
+        }
+        
+        return sample(OCCUPATIONS);
+    }
+
+    setNewMoveTimer() {
+        this.moveTimer = randomInt(3000, 8000);
+        this.targetX = this.x + randomInt(-100, 100);
+        this.targetY = this.y + randomInt(-100, 100);
+        
+        // Keep within town boundaries if assigned to a town
+        if (this.town) {
+            const maxDistance = 180; // Town radius - some margin
+            const dx = this.targetX - this.town.x;
+            const dy = this.targetY - this.town.y;
+            const distance = Math.sqrt(dx * dx + dy * dy);
+            
+            if (distance > maxDistance) {
+                const scale = maxDistance / distance;
+                this.targetX = this.town.x + dx * scale;
+                this.targetY = this.town.y + dy * scale;
+            }
+        }
+    }
+
+    generateThought() {
+        const thoughts = [
+            'HUNGRY', 'TIRED', 'HAPPY', 'SAD', 'WORK',
+            'PLAY', 'SHOP', 'SOCIALIZE', 'LEARN', 'BUILD'
+        ];
+        
+        // Weight thoughts based on current state
+        const weightedThoughts = thoughts.filter(thought => {
+            switch (thought) {
+                case 'WORK':
+                    return this.age >= 13 && !this.isPlayingTag && !this.isPlayingRPS;
+                case 'PLAY':
+                    return this.age < 18 || this.isPlayingTag || this.isPlayingRPS;
+                case 'SHOP':
+                    return this.age >= 15 && this.town && this.town.buildings.some(b => b.type === 'store');
+                case 'BUILD':
+                    return this.occupation === 'Builder';
+                default:
+                    return true;
+            }
+        });
+        
+        return getThought(sample(weightedThoughts));
+    }
+
+    generateTraits() {
+        const traits = new Set();
+        const traitList = Object.values(TRAITS);
+        
+        // Each person has a 20% chance for each trait
+        traitList.forEach(trait => {
+            if (Math.random() < 0.2) {
+                traits.add(trait);
+            }
+        });
+        
+        return traits;
     }
 
     // Add all the Person class methods here...
